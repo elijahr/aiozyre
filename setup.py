d7ca38b8cfd0c0956673c0d3b660935fd3b1ef05
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-01-03 14:00:57 -0600

    Don’t require cython

--- setup.py
+++ setup.py
@@ -4,33 +4,36 @@ from setuptools import setup, Extension
 try:
     from Cython.Build import cythonize
 except ImportError:
-    def cythonize(*args, **kwargs):
-        from Cython.Build import cythonize
-        return cythonize(*args, **kwargs)
+    # Cython not available, build from C
+    mod_ext = 'c'
+
+    def cythonize(extensions, *args, **kwargs):
+        return extensions
+else:
+    mod_ext = 'pyx'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='aiozyre',
-    version='1.0.1',
+    version='1.0.2',
     description='asyncio-friendly Python bindings for Zyre',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Elijah Shaw-Rutschman',
     author_email='elijahr+aiozyre@gmail.com',
     packages=['aiozyre'],
-    setup_requires=[
-        'cython',
-    ],
+    install_requires=['cython'],
+    setup_requires=['cython'],
     ext_modules=cythonize([
-        Extension('aiozyre.futures', sources=['aiozyre/futures.pyx']),
-        Extension('aiozyre.node', sources=['aiozyre/node.pyx']),
-        Extension('aiozyre.nodeactor', sources=['aiozyre/nodeactor.pyx'], libraries=['czmq', 'zyre']),
-        Extension('aiozyre.nodeconfig', sources=['aiozyre/nodeconfig.pyx']),
-        Extension('aiozyre.signals', sources=['aiozyre/signals.pyx']),
-        Extension('aiozyre.util', sources=['aiozyre/util.pyx'], libraries=['czmq', 'zyre']),
-        Extension('aiozyre.zyre', sources=['aiozyre/zyre.pyx'], libraries=['czmq', 'zyre']),
+        Extension('aiozyre.futures', sources=['aiozyre/futures.%s' % mod_ext]),
+        Extension('aiozyre.node', sources=['aiozyre/node.%s' % mod_ext]),
+        Extension('aiozyre.nodeactor', sources=['aiozyre/nodeactor.%s' % mod_ext], libraries=['czmq', 'zyre']),
+        Extension('aiozyre.nodeconfig', sources=['aiozyre/nodeconfig.%s' % mod_ext]),
+        Extension('aiozyre.signals', sources=['aiozyre/signals.%s' % mod_ext]),
+        Extension('aiozyre.util', sources=['aiozyre/util.%s' % mod_ext], libraries=['czmq', 'zyre']),
+        Extension('aiozyre.zyre', sources=['aiozyre/zyre.%s' % mod_ext], libraries=['czmq', 'zyre']),
     ]),
     classifiers=[
         'Development Status :: 3 - Alpha',
